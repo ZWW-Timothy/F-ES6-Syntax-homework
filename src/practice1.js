81250bff36bfe6c3105c8554876ac5e30415a335
ZW <zhaowuwei2020@foxmail.com>
2020-11-13 15:03:06 +0800

    [ZWW]: Finish practice 1.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
+const parseData = ({data, column}) => {
+    return data.map((dataValue) => {
+        const dataObject = {};
+        column.map((columnObject) => columnObject.name)
+        .forEach((name, index) => {   
+            dataObject[name] = dataValue[index];         
+        });
+        return dataObject;
+    });
+}
+
 export { parseData };
