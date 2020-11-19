7ce194138eabfd09b4ee81b4d0744de1de72340f
ZW <zhaowuwei2020@foxmail.com>
2020-11-19 15:06:07 +0800

    [ZWW]: Finish practice 2.

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
+const inject = (stringData, objectData) => {
+    const result = [];
+    for (let i = 0; i < stringData.length; i += 1) {
+        const objectTemp = objectData.find(object => object.index === i);
+        if (objectTemp) {
+            result.push(objectTemp.content);
+        }
+        result.push(stringData[i]);
+    }
+    return result;
+}
+
 export { inject };
